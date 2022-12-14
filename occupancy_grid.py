62d4096d3746f4d3fb2441dbca962efcdd82d775
femartip <felix.marti99@gmail.com>
2022-12-14 14:16:19 +0100

    Position of mannequins

--- occupancy_grid.py
+++ occupancy_grid.py
@@ -1,29 +1,38 @@
+import math
+import time
 import rospy as ros
 import std_msgs.msg
 from nav_msgs.msg import OccupancyGrid
 from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
+from typing import Union
+from rospy import Subscriber
+import tf
+from geometry_msgs.msg import PoseStamped
 
 class occupancy_grid:
 
-    def data2matrix(data):
+    def data2matrix(self, data):
         print("Converting data to matrix")
         info = data.info
-        robot_x = info.origin.position.x
-        robot_y = info.origin.position.y
+        self.robot_x = info.origin.position.x
+        self.robot_y = info.origin.position.y
         d = data.data
-        width = info.width
-        height = info.height
-        for i in range(width):
-            for j in range(height):
-                if d[i*width + j] != -1:
-                    map_matrix[(i,j)] = d[i*width + j]
-        map_matrix[(width/2 - int(robot_x),height/2 - int(robot_y))]= 200
-        xmin=width
+        self.width = info.width
+        self.height = info.height
+        for i in range(self.width):
+            for j in range(self.height):
+                if d[i*self.width + j] != -1:
+                    self.map_matrix[(i,j)] = d[i*self.width + j]
+        #print("Robot pos: {}, {}".format(self.robot_x,self.robot_y))
+        #print("Matrix robot pos: {}, {}".format(self.width/2 - int(self.robot_x),self.height/2 - int(self.robot_y)))
+        self.map_matrix[(self.width/2 - int(self.robot_x),self.height/2 - int(self.robot_y))]= 200
+        xmin=self.width
         xmax=0
-        ymin=height
+        ymin=self.height
         ymax=0
-        print(robot_x, robot_y)
-        for (i,j) in map_matrix.keys():
+        print(self.robot_x, self.robot_y)
+        for (i,j) in self.map_matrix.keys():
             if (i < xmin):
                 xmin = i
             if (i > xmax):
@@ -34,29 +43,63 @@ class occupancy_grid:
                 ymax = j
         for i in range(xmin,xmax):
             for j in range(ymin,ymax):
-                if (i,j) not in map_matrix:
+                if (i,j) not in self.map_matrix:
                     print(" ", end = "")
-                elif map_matrix[(i,j)] == 0:
+                elif self.map_matrix[(i,j)] == 0:
                     print("-", end = "")
-                elif map_matrix[(i,j)] == 100:
+                elif self.map_matrix[(i,j)] == 100:
                     print("#", end = "")
-                elif map_matrix[(i,j)] == 200:
+                elif self.map_matrix[(i,j)] == 200:
                     print("@", end = "")
+                if len(self.mannequins) != 0:
+                    if (i,j) in self.mannequins:
+                        print("M", end = "")
+                    
             print('') 
 
-    def mannequins2grid(data):
-        for i in range(len(data)):
-            angle1 = data[i]
-            angle2 = data[i+1] 
+    def mannequins2grid(self,data):
+        mannequins = list(data.data)
+        m = 0
+        for i in range(0,len(mannequins)-1,2):
+            angle = mannequins[i]
+            distance = mannequins[i+1]
+            # Calculate the manhatten distance split into x and y
+            x = int(distance * math.cos(angle))
+            y = int(distance * math.sin(angle))
+            try:
+                rmx = int(self.robot_x + x)
+                rmy = int(self.robot_y + y)
+                print("Mannequin {}: {},{}".format(m,rmx,rmy))
+                mx,my = self.width/2 - rmx, self.height/2 - rmy
+                self.mannequins.append((mx,my))
+                print("Mannequin in matrix {}: {},{}".format(m,mx,my))
 
-    def listener():
+                self.map_matrix[(mx,my)] = 'M'
+                #print(self.map_matrix[(mx,my)])
+            except:
+                print("Mannequin out of range")
+            # Calculate the distance in the map
+            m+=1
+            #print("Distance of mannequin {}: {},{}".format(m,x,y))
+            print("------------------------------------------------------------------------------------")
+
+
+    def __init__(self):
+        self.map_matrix = {}
+        self.width = 0
+        self.height = 0
+        self.robot_x = 0
+        self.robot_y = 0
+        self.mannequins = []
         print("Listening to map topic")
-        ros.Subscriber("map", OccupancyGrid, occupancy_grid.data2matrix)
-        ros.Subscriber("mannequin", numpy_msg, occupancy_grid.mannequins2grid)
-        ros.spin()
+        ros.Subscriber("map", OccupancyGrid, self.data2matrix)
+        #time.sleep(2)
+        ros.Subscriber("mannequins", numpy_msg(Floats), self.mannequins2grid)
+        
+        
 
-    
-ros.init_node('occupancy_grid', anonymous = False)
-#rate = ros.Rate(10) 
-map_matrix = {}
-occupancy_grid.listener()
+if __name__ == '__main__':  
+    ros.init_node('occupancy_grid')
+    #rate = ros.Rate(10) 
+    occupancy_grid()
+    ros.spin()
