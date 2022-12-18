a40c37bd90b9820e8ab56b60bf098c16acfd8e08
femartip <felix.marti99@gmail.com>
2022-12-18 16:32:33 +0100

    run

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # This script is used to run the project, but does not work yet!!!!!
-python3 mannequin_detector_old.py 
-python3 mannequin_coordinates.py 
+python3 mannequin_detector_old.py &
+positions.py &
+my_test_goals.py &
+
 
 
 
